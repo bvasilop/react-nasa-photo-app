b18d825b241f15b351e7a65006d4c144ba5ae6c0
bvasilop <bvasilop@gmail.com>
2020-03-14 12:58:59 -0700

    Updates

---
+++ precache-manifest.f7c2c30cfd1a9910f334a87fcebe2491.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "172a0a5b072fdd82d8f409d765645579",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96e44713af16f7d3bd73",
+    "url": "/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "cb35222141a2912b3942",
+    "url": "/static/css/main.d5e16e9f.chunk.css"
+  },
+  {
+    "revision": "96e44713af16f7d3bd73",
+    "url": "/static/js/2.e712fb3f.chunk.js"
+  },
+  {
+    "revision": "a52d65c95a2d7ffcb9ff212c4d38dff4",
+    "url": "/static/js/2.e712fb3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb35222141a2912b3942",
+    "url": "/static/js/main.2b5bb639.chunk.js"
+  },
+  {
+    "revision": "5ac1d3671cfa24c8e277",
+    "url": "/static/js/runtime-main.14e812fc.js"
+  }
+]);
