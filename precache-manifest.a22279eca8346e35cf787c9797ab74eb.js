26f513beb50569c383c401029ddec4bd2668ff11
bvasilop <bvasilop@gmail.com>
2020-03-14 13:04:03 -0700

    Updates

---
+++ precache-manifest.a22279eca8346e35cf787c9797ab74eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "650172560c1520a7b5fc8e0c090496e2",
+    "url": "/react-nasa-photo-app/index.html"
+  },
+  {
+    "revision": "96e44713af16f7d3bd73",
+    "url": "/react-nasa-photo-app/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "a3e3dc60537ea1d66b2e",
+    "url": "/react-nasa-photo-app/static/css/main.d5e16e9f.chunk.css"
+  },
+  {
+    "revision": "96e44713af16f7d3bd73",
+    "url": "/react-nasa-photo-app/static/js/2.e712fb3f.chunk.js"
+  },
+  {
+    "revision": "a52d65c95a2d7ffcb9ff212c4d38dff4",
+    "url": "/react-nasa-photo-app/static/js/2.e712fb3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3e3dc60537ea1d66b2e",
+    "url": "/react-nasa-photo-app/static/js/main.ccfc6bf1.chunk.js"
+  },
+  {
+    "revision": "63549a666dc6d13bc61c",
+    "url": "/react-nasa-photo-app/static/js/runtime-main.9651f45f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f7c2c30cfd1a9910f334a87fcebe2491.js"
+  "/react-nasa-photo-app/precache-manifest.a22279eca8346e35cf787c9797ab74eb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-nasa-photo-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
